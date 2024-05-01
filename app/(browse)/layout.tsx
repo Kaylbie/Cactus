8a8c4f009d8fb7e7784317a846dfec05bd028c66
kaylbie <technologyhero2014@gmail.com>
2024-05-02 01:24:31 +0300

    Side nav update

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,6 +1,7 @@
+import { Suspense } from "react";
 import { Container } from "./_components/container";
 import { NavBar } from "./_components/navbar";
-import { Sidebar } from "./_components/sidebar";
+import { Sidebar, SidebarSkeleton } from "./_components/sidebar";
 
 const BrowseLayout = ({
     children,
@@ -11,7 +12,10 @@ const BrowseLayout = ({
         <>
             <NavBar />
             <div className="flex h-full pt-20">
+                <Suspense fallback={<SidebarSkeleton />}>
                 <Sidebar />
+                </Suspense>
+                
                 <Container>
                     {children}
                 </Container>
