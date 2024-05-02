052a64a967ae4231ca970dd2207ac9073e7dd413
kaylbie <technologyhero2014@gmail.com>
2024-05-02 11:43:17 +0300

    Following features

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from '@/components/theme-provider';
-
+import {Toaster} from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -22,11 +22,13 @@ export default function RootLayout({
     
       <html lang="en">
         <body className={inter.className}>
+          
           <ThemeProvider
             attribute="class"
             forcedTheme="dark"
             storageKey="cactus-theme"
           >
+            <Toaster theme="light" position="bottom-center"/>
             {children}
           </ThemeProvider>
           
--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -3,10 +3,36 @@ import {db} from "@/lib/db";
 import {getSelf} from "@/lib/auth-service";
 
 export const getRecommendedService = async()=>{
-    const users=await db.user.findMany({
-        orderBy:{
-            createdAt:"desc"
-        },
-    });
+    //await new Promise((resolve)=>setTimeout(resolve,1000));
+    let userId;
+    try{
+        const self=await getSelf();
+        userId=self.id;
+    }catch{
+        userId=null;
+    }
+    let users=[];
+
+    if(userId){
+        users=await db.user.findMany({
+            where:{
+                NOT:{
+                    id:userId
+                },
+            },
+            orderBy:{
+                createdAt:"desc"
+            },
+            
+        });
+
+    }else{
+        users=await db.user.findMany({
+            orderBy:{
+                createdAt:"desc"
+            },
+        });
+    }
+    
     return users;
 };
